eb09557e770458a68204910e88cfda7480c76b0b
Ubuntu <ubuntu@ip-172-31-7-60.ap-northeast-1.compute.internal>
2021-04-21 04:53:20 +0000

    work on lesson5 completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,48 @@
 #write your code here
+# # Simon Says
+#
+# ## Topics
+#
+# * functions
+# * strings
+# * default parameter values
+#
+# ## Hints
+#
+# When you make the second `repeat` test pass, you might break the **first**
+#
+def echo input
+	input
+end
+
+def shout input
+	input.upcase
+end
+
+def repeat input, repeat=2
+	(input + " ")*(repeat - 1) + input
+end
+
+def start_of_word input, n
+	input[0, n]
+end
+
+def first_word input
+	input.split(" ")[0]
+end
+
+def titleize input
+	array = input.split(" ")
+	count = 0
+	array.each do |word|
+		if count == 0
+			word.capitalize!
+			count = 1
+		elsif word == "and" || word == "over" || word == "the"
+			word.downcase!
+		else
+			word.capitalize!
+		end
+	end
+	array.join(" ")
+end
